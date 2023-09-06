f6d4d861a4b00459a1091b4172047f81457df7e1
Ryan Harmon <ryan@bloomcu.com>
2023-09-06 15:40:27 -0600

    Display error if duplicate column or row UID is found

--- app/Http/CSV/CSVController.php
+++ app/Http/CSV/CSVController.php
@@ -17,16 +17,30 @@ class CSVController extends Controller
 {
     public function show(Organization $organization, File $file)
     {
-        // Setup CSV
+        // Setup CSV from file
         $csvFile = Storage::get($file->path);
         $csv = Reader::createFromString($csvFile);
-        $csv->setHeaderOffset(0);
+
+        // Check for duplicate headers
+        $header = $csv->fetchOne(0);
+        if (collect($header)->duplicates()->count()) {
+            // TODO: Create an exception class for this
+            return response()->json([
+                'message' => 'There is a problem with this CSV file.',
+                'errors' => [
+                    'uid' => ['The CSV contains duplicate columns using the same Unique ID.']
+                ],
+            ], 200);
+        }
         
-        // Separate header from rows
+        // Setup header and rows
+        // Header: Each column uid as key and column name as value
+        // Rows: Each cell with column uid as key and cell content as value
+        $csv->setHeaderOffset(0);
         $csv = collect($csv);
         $csvHeader = collect($csv[1]);
         $csvRows = collect($csv)->slice(1);
-        
+
         // Validate header contains the top left "Unique ID" column
         if (!$csvHeader->has('Unique ID')) {
             // TODO: Create an exception class for this
@@ -86,6 +100,17 @@ class CSVController extends Controller
                     ],
                 ], 200);
             }
+        }
+
+        // Check for duplicate rows
+        if ($rows->duplicates('uid')->count()) {
+            // TODO: Create an exception class for this
+            return response()->json([
+                'message' => 'There is a problem with this CSV file.',
+                'errors' => [
+                    'uid' => ['The CSV contains duplicate rows using the same Unique ID.']
+                ],
+            ], 200);
         }
 
         return response()->json([
